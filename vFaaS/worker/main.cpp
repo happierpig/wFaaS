23333ef62d34175f7ed30fa06fa2b3518314b28a
happierpig <zhaoyilong217@sjtu.edu.cn>
2023-02-13 20:51:29 +0800

    [Fix] File descrpitor collides with stdout in Forking.

--- vFaaS/worker/main.cpp
+++ vFaaS/worker/main.cpp
@@ -1,18 +1,19 @@
 #include "worker.hpp"
+#include <stdlib.h>
 
 int main(){
+    srand(time(NULL));
     wasrModule wasmRuntime;
     int _inputSize = wasmRuntime.getInputSize(), _returnSize = wasmRuntime.getReturnSize();
     auto inputBuffer = new unsigned char[_inputSize];
     auto returnBuffer = resultBuffer;
     while(true){
-        wasmRuntime.constructRuntime();
         PIPE_COMMAND cmd = util::readPIPECommand(0);
         util::readBytes(0, inputBuffer, _inputSize);
         wasmRuntime.runWasmCode(inputBuffer);
-        util::writePIPECommand(1, PIPE_COMMAND_RETURN);
-        write(1, returnBuffer, _returnSize);
-        wasmRuntime.deconstructRuntime();
+        cmd = PIPE_COMMAND_RETURN;
+        util::writePIPECommand(10, cmd);
+        write(10, returnBuffer, _returnSize);
     }
     delete[] inputBuffer;
     return 0;
@@ -25,9 +26,14 @@ int main(){
 //     int x = 1,y = 2;
 //     std::copy((uint8*)(&x), ((uint8*)(&x)+4),inputBuffer);
 //     std::copy((uint8*)(&y), ((uint8*)(&y)+4),inputBuffer+4);
-//     wasmRuntime.constructRuntime();
-//     wasmRuntime.runWasmCode(inputBuffer);
-//     wasmRuntime.deconstructRuntime();
-//     printf("The result being set is %d.",(*(reinterpret_cast<int*> (resultBuffer))));
+//     fstream tmpfile; tmpfile.open("/home/dreamer/epcc/vFaaS/test.txt", std::ios::out | std::ios::in);
+//     for(int i = 1;i < 10;++i){
+//         wasmRuntime.runWasmCode(inputBuffer);
+//         printf("The result being set is %d.",(*(reinterpret_cast<int*> (resultBuffer))));
+//     }
+//     srand(time(NULL));
+//     int id = rand();
+//     std::cout << "Identifier Number: " << id << std::endl;
+//     tmpfile << id;
 //     return 0;
 // }
